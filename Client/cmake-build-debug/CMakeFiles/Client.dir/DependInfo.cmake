6cb99de0038dc69b77d6986a78732b40ea8351dd
ldy <s230026071@mail.uic.edu.cn>
2024-05-09 17:28:16 +0800

    Revert to "implemented sample program".

---
+++ Client/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Programme/DCN_chat_program/Client/client.cpp" "CMakeFiles/Client.dir/client.cpp.obj" "gcc" "CMakeFiles/Client.dir/client.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
