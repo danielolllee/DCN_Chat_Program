a658066b4a5ca378141dcbdc1cdd086ed1415960
muqilii <wqbknsyczdxx48@gmail.com>
2024-05-09 23:41:57 +0800

    add multiple thread to the server

--- Client/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
+++ Client/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -8,11 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "D:/Programme/DCN_chat_program/Client/client.cpp" "CMakeFiles/Client.dir/client.cpp.obj" "gcc" "CMakeFiles/Client.dir/client.cpp.obj.d"
+  "C:/Users/avery/Desktop/DCN/DCN_chat_program/Client/client.cpp" "CMakeFiles/Client.dir/client.cpp.obj" "gcc" "CMakeFiles/Client.dir/client.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
 set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
